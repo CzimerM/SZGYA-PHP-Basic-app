ef2c54a8fd319db423e37555f346146e2e4dbf4b
Csizmadia Márk <airamek@teamorange.hu>
2024-06-05 09:00:12 +0200

    delete error handling

--- torles.php
+++ torles.php
@@ -1,7 +1,14 @@
 <?php
 require("kapcsolat.php");
 $sql = "DELETE FROM dolgozok WHERE id like {$_GET['id']}";
-$dbconn->query($sql);
-header("Location: lista.php");
-exit();
+if($dbconn->query($sql))
+{
+    header("Location: lista.php");
+    exit();
+}
+else
+{
+    echo("Error description: " . $dbconn->error);
+}
+
 ?>
