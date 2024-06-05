50f76d98faefcd02a78c88717cbfd6656a30ecc3
Csizmadia Márk <airamek@teamorange.hu>
2024-06-05 09:02:49 +0200

    more err handling

--- torles.php
+++ torles.php
@@ -1,5 +1,11 @@
 <?php
 require("kapcsolat.php");
+if (!isset($_GET["id"]))
+{
+    echo "Id paraméter hiányzik";
+    die();
+}
+
 $sql = "DELETE FROM dolgozok WHERE id like {$_GET['id']}";
 if($dbconn->query($sql))
 {
